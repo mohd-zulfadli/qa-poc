e03a87de50ce454fe11401e1da8348f81a55cc6e
mohd-zulfadli <mohdzulfadli@gmail.com>
2026-03-09 13:21:50 +0800

    PerfTest.scala update to support gatling perf test parameters

--- karate-tests/src/test/scala/com/example/PerfTest.scala
+++ karate-tests/src/test/scala/com/example/PerfTest.scala
@@ -3,6 +3,7 @@ package com.example
 import com.intuit.karate.gatling.PreDef._
 import io.gatling.core.Predef._
 import scala.concurrent.duration._
+import scala.language.postfixOps
 
 class PerfTest extends Simulation {
 
